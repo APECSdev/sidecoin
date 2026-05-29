d4be5818f6dd422eea8f428ffca0135d93f84050
Shomari <nyusternie@sdot.io>
2026-05-29 11:42:36 -0400

    Add SoLoader call.

--- packages/mobile/android/app/src/main/java/app/sidecoin/MainApplication.kt
+++ packages/mobile/android/app/src/main/java/app/sidecoin/MainApplication.kt
@@ -11,6 +11,7 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
@@ -39,6 +40,7 @@ class MainApplication : Application(), ReactApplication {
 
     override fun onCreate() {
         super.onCreate()
+        SoLoader.init(this, false)
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
             load()
         }
